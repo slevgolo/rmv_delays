908e408bf197e8ab94bf88df2f68acbf8701717b
slevink94 <marcelr94@gmx.de>
2021-02-14 13:07:28 +0100

    perform API request (multithread) store results in dict write results to delays.csv simple logging in delays.log

--- delays.py
+++ delays.py
@@ -1,4 +1,75 @@
+import concurrent.futures
+import logging
+import os
+import re
+import time
+from datetime import datetime
+
+import pandas as pd
 import requests
+
+
+def replace_umlaute(astring):
+    unicodes = {
+        'Ä': '&#196;',
+        'ä': '&#228;',
+        'Ö': '&#214;',
+        'ö': '&#246;',
+        'Ü': '&#220;',
+        'ü': '&#252;',
+        'ß': '&#223;'
+    }
+
+    for key, value in unicodes.items():
+        astring = astring.replace(value, key)
+    return astring
+
+
+def re_findall(key, txt):
+    result = re.findall(f'"{key}":"([^"]*)', txt)
+    if len(result) == 0:
+        result = re.findall(f'"{key}":([^,]*)', txt)
+    return result[0]
+
+
+def createLogger():
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(
+        filename="delays.log",
+        level=logging.INFO,
+        format="[%(asctime)s]:%(levelname)s:%(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S"
+    )
+    logger = logging.getLogger(__name__)
+    stream_handler = logging.StreamHandler()
+    formatter = logging.Formatter(
+        "[%(asctime)s]:%(filename)s:%(levelname)s:%(message)s", datefmt="%Y-%m-%d %H:%M:%S")
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+    return logger
+
+
+def getDelays(station, url):
+    result_string = requests.get(url).text
+    result_string = result_string.replace(' ', '')
+    result_string = replace_umlaute(result_string)
+    start_id = result_string.find('[')
+    end_id = result_string.rfind(']')
+    result_string = result_string[start_id+1:end_id]
+
+    entries = result_string.splitlines()
+
+    keys = ['date', 'time',  'status', 'progTime',
+            'progDelay', 'name', 'direction', 'platform']
+    dic = {key: [] for key in keys}
+    for entry in entries:
+        if 'DELAYED' in entry:
+            if int(re_findall('progDelay', entry)) > 10:
+                dic['station'] = station
+                for key in keys:
+                    dic[key].append(re_findall(key, entry))
+
+    return pd.DataFrame.from_dict(dic)
 
 # urls of rmv api anzeigetafel
 current_time = re.findall(r'\d\d:\d\d:\d\d', str(datetime.now()))[0]
@@ -18,4 +89,26 @@ stations = [
     'Mainz Am Schinnergraben', 
     'Mainz Im Münchfeld'
     ]
-	
+
+logger = createLogger()
+
+t1 = time.perf_counter()
+logger.info("Start execution!")
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    res = [executor.submit(getDelays, station, url)
+           for url, station in zip(urls, stations)]
+logger.info("It took {:.2f} seconds to check {} urls.".format(
+    time.perf_counter()-t1, len(urls)))
+
+# create single dataframe out of all the dataframes
+df = pd.concat([r.result() for r in res], ignore_index=True, sort=False)
+logger.info("Number of delayed rides: {}.".format(df.shape[0]))
+
+# save dataframe
+if df.shape[0] > 0:
+    file_name = 'delays.csv'
+    if not os.path.exists(file_name):
+        df.to_csv(file_name, index=False)
+        
+    df.to_csv(file_name, header=None, index=False, mode='a')
+    logger.info(f"Saved results in {file_name}.")
